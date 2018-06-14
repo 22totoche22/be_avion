7cad4992a3dc6427544a5c8766e6e641b690b819
DenisAll <allandenis974@gmail.com>
2018-06-14 12:17:10 +0200

    [seance3] trajectories of linear and non linear models

--- seance3.py
+++ seance3.py
@@ -15,7 +15,7 @@ import pdb
 
 
 def points_trim():
-    h = [3000.0, 10000.0]  # altitude en m
+    h = [3000, 10000]  # altitude en m
     Ma = [0.4, 0.9]  # intervalel nombre de mach
     ms = [-0.2, 0.5]  # marge statique
     km = [0.1, 1]  # coefficient de réglage de la masse
@@ -29,8 +29,8 @@ def points_trim():
 
 
 
-choix = np.random.random_integers(0, len(points_trim())-1)
-point = points_trim()[choix]
+
+point = points_trim()[15]
 alt, Ma, sm, km = point
 
 
@@ -44,41 +44,63 @@ def nonLinearModel(aircraft, alt, Ma, sm, km, wh, time):
 
 
 
-
-aircraft = dyn.Param_A321()
-wh = 2 #m/s
-
-time = np.arange(0., 240, 0.1)
-XnonLin = nonLinearModel(aircraft, alt, Ma, sm, km, wh, time)
-fig = plt.figure()
-dyn.plot(time, XnonLin, figure = fig)
-
-plt.show()
-
 ''' Modèle linéaire '''
 
 
-def dynlin(X, t, A, B, U):
-    xdot = np.dot(A,X) + np.dot(B,U)
-    return xdot
+def dynlin(dX, t, A, B, dU):
+    dxdot = np.dot(A,dX) + np.dot(B,dU)
+    return dxdot
 
 
 
 
 
 def LinearModel(aircraft, alt, Ma, sm, km, wh, time):
+
     aircraft.set_mass_and_static_margin(km, sm)
     va = dyn.va_of_mach(Ma, alt)
-    xtrim, utrim = dyn.trim(aircraft, {'va':va, 'h':alt})
-    xtrim[dyn.s_a] = xtrim[dyn.s_a] + np.arctan(wh/xtrim[dyn.s_va])
+    xtrim, utrim = dyn.trim(aircraft, {'va':va,'gamm':0, 'h':alt})
+    xtrim[dyn.s_a] = xtrim[dyn.s_a] + np.arctan(wh / xtrim[dyn.s_va])
+    wing = [0] * 6
     A, B = utils.num_jacobian(xtrim, utrim, aircraft, dyn.dyn)
-    X = scipy.integrate.odeint(dynlin, xtrim, time, args=(A, B, utrim))
+    du = np.zeros((4,))
+    wing[dyn.s_a] += np.arctan(wh / xtrim[dyn.s_va])
+    dX = scipy.integrate.odeint(dynlin, wing, time, args=(A, B, du))
+    X = dX+xtrim
+    for i in range(len(X)):
+        X[i][dyn.s_y] = X[i][dyn.s_va]*time[i]
     return X
 
 
+''' Affichages des trajectoires '''
+
+aircraft = dyn.Param_A321()
+wh = 2 #m/s
+time = np.arange(0, 240, 0.1)
+delta_t = (time[1]-time[0])/len(time)
+
+
+
+XnonLin = nonLinearModel(aircraft, alt, Ma, sm, km, wh, time)
+fignonLin = plt.figure()
+dyn.plot(time, XnonLin, figure=fignonLin)
+
+
+
+
 XLin = LinearModel(aircraft, alt, Ma, sm, km, wh, time)
-fig = plt.figure()
-dyn.plot(time, XLin, figure = fig)
+figLin = plt.figure()
+dyn.plot(time, XLin, figure=figLin)
+
+
+
+plt.show()
+
+
+figDiff = plt.figure()
+dyn.plot(time, abs(XLin - XnonLin), figure=figDiff)
+
+
 plt.show()
 
 
